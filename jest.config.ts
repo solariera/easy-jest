0e8aadd5e00b861298e7c2037b016d4a8da97577
Always <alwaysrusting@gmail.com>
2021-06-25 00:12:06 +0900

    000003 [fix] jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  clearMocks: true,
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  testMatch: ['**/*.spec.ts', '**/*.spec.tsx'],
+};
+
+export default config;
--- src/convertString/convertString.spec.ts
+++ src/convertString/convertString.spec.ts
@@ -1,12 +1,17 @@
 import { tests, TestData } from '../tests';
-import { convertString as fn } from './';
+import { convertString as fn } from '.';
 
 const data: TestData<typeof fn>[] = [
+  { params: ['no id and no mode'], ret: 'no id and no mode' },
   { id: 'undefined', params: [undefined], ret: 'undefined', mode: 'toBe' },
   { id: 'null', params: [null], ret: 'null', mode: 'toBe' },
+  { id: 'boolean - true', params: [true], ret: 'true', mode: 'toBe' },
+  { id: 'boolean - false', params: [false], ret: 'false', mode: 'toBe' },
   { id: 'string', params: ['hogehoge'], ret: 'hogehoge', mode: 'toBe' },
   { id: 'number - 0', params: [0], ret: '0', mode: 'toBe' },
   { id: 'number - 1', params: [1], ret: '1', mode: 'toBe' },
+  { id: 'array', params: [[0, 1, 2]], ret: '[0,1,2]', mode: 'toEqual' },
+  { id: 'object', params: [{ key: 'a01', value: 1 }], ret: '{"key":"a01","value":1}', mode: 'toStrictEqual' },
 ];
 
 tests(fn, data);
